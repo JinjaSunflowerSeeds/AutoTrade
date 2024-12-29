1acd25fbfe2ec919a93bb6a7dba2476adb24a5bf
Ragheb Rahmaniani <maheesaragheb@gmail.com>
2024-12-29 09:27:14 -0800

    interactive plot update minor

--- src/visualizer/candles.py
+++ src/visualizer/candles.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.pyplot as plt
 import mplfinance as mpf
 import numpy as np
@@ -6,16 +7,31 @@ from matplotlib.backend_bases import MouseEvent
 
 
 class InteractiveCandlestickChart:
-    def __init__(self, filepath, label):
+    def __init__(self, filepath, outputfile, ticker, label):
         self.label = label
         # Load and prepare the data
         self.df = pd.read_csv(filepath).tail(50)
         self.df["date"] = pd.to_datetime(self.df["date"])
         self.df.set_index("date", inplace=True)
+        #
+        self.outputfile = outputfile.format(
+            stock=ticker,
+            label=self.label,
+            start_date=self.df.index[0].strftime("%Y_%m_%d_%H_%M"),
+            end_date=self.df.index[-1].strftime("%Y_%m_%d_%H_%M"),
+        )
+        if not os.path.exists(self.outputfile):
+            os.makedirs(os.path.dirname(self.outputfile), exist_ok=True) 
+        #
+        self.fig = None
+        self.ax = None
+        self.ax_vol = None
+        #
+        self.labels = []
         # Storage for labels
         self.labels = []
         self.draw_plot()
-        
+
     def draw_plot(self):
         # Create the plot (with two subplots
         self.fig, (self.ax, self.ax_vol) = plt.subplots(
@@ -42,9 +58,9 @@ class InteractiveCandlestickChart:
     def get_df_info(self, x_coord, x_dates):
         step = (self.ax.get_xlim()[1] - self.ax.get_xlim()[0]) / len(x_dates)
         x = x_coord - self.ax.get_xlim()[0]
-        idx = int(np.ceil(x / step)-1)
+        idx = int(np.ceil(x / step) - 1)
         clicked_date = x_dates[idx]
-        low_price = self.df[self.df.index==clicked_date]["low"].values[0]
+        low_price = self.df[self.df.index == clicked_date]["low"].values[0]
         print(f"Clicked on: {clicked_date} (Idx: {idx}), (low price is {low_price})")
         return clicked_date, idx, low_price
 
@@ -64,16 +80,16 @@ class InteractiveCandlestickChart:
             print("Clicked out of range.")
             return
         date, idx, low_price = self.get_df_info(x_coord, x_dates)
-        self.labels.append({"date": date,  "label": self.label})
+        self.labels.append({"date": date, "label": self.label})
         self.save_labels_to_file()
-        # 
+        #
         line = self.ax.axvline(x=event.xdata, color="black", linestyle="--")
         plt.draw()
 
     def save_labels_to_file(self):
         labels_df = pd.DataFrame(self.labels)
-        labels_df.to_csv(f"manual_labels_{self.label}.csv", index=False)
-        print(f"Labels saved to manual_labels_{self.label}.csv.")
+        labels_df.to_csv(self.outputfile, index=False)
+        print(f"Labels saved to  {self.outputfile}.")
 
     def show(self):
         self.fig.tight_layout()
@@ -81,8 +97,10 @@ class InteractiveCandlestickChart:
         plt.show()
 
 
-# Usage example
-filepath = "/Users/ragheb/myprojects/stock/src/files/training/fe_1d.csv"
-# outputpath= f"/Users/ragheb/myprojects/stock/src/files/training/manual_labels/{}/{self.label}_{}_to_{}.csv"
-chart = InteractiveCandlestickChart(filepath, "Buy")
-chart.show()
+if __name__ == "__main__":
+    # Usage example
+    ticker = "NVDA"
+    filepath = "/Users/ragheb/myprojects/stock/src/files/training/fe_1d.csv"
+    outputfile = "/Users/ragheb/myprojects/stock/src/files/training/manual_labels/{stock}/{label}_{start_date}_to_{end_date}.csv"
+    chart = InteractiveCandlestickChart(filepath, outputfile, ticker, "Buy")
+    chart.show()
